6c6803c0144b709cc8ba0ef591f35fe0e4cc3a2b
ArnabKhan07 <arnabsuvokhan@gmail.com>
2024-06-15 22:48:15 +0530

    Add files via upload

---
+++ DoublyNode.java
@@ -0,0 +1,12 @@
+public class DoublyNode {
+
+    public Integer data;
+    public DoublyNode prev;
+    public DoublyNode next;
+
+    DoublyNode(Integer data) {
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+}
